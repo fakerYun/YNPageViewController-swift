7513c6347503a1a773f7e4c03788e9d93e8c2c32
yun <515641234@qq.com>
2022-03-01 10:04:03 +0800

    Initial Commit

---
+++ YNPageViewController-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YNPageViewController-swift
+//
+//  Created by Yun Wang 王云 on 2022/3/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YNPageViewController-swiftUITests/YNPageViewController_swiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  YNPageViewController_swiftUITestsLaunchTests.swift
+//  YNPageViewController-swiftUITests
+//
+//  Created by Yun Wang 王云 on 2022/3/1.
+//
+
+import XCTest
+
+class YNPageViewController_swiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
